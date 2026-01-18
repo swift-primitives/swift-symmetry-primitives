b15dc22d748345082852283ca5c4ec952f4ac49f
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-18 20:13:57 +0100

    Migrate tests to nested package pattern

--- Package.swift
+++ Package.swift
@@ -9,20 +9,19 @@ let package = Package(
         .iOS(.v26),
         .tvOS(.v26),
         .watchOS(.v26),
-        .visionOS(.v26),
+        .visionOS(.v26)
     ],
     products: [
         .library(
             name: "Symmetry Primitives",
             targets: ["Symmetry Primitives"]
-        ),
+        )
     ],
     dependencies: [
         .package(path: "../swift-algebra-linear-primitives"),
         .package(path: "../swift-affine-primitives"),
         .package(path: "../swift-dimension-primitives"),
-        .package(path: "../swift-numeric-primitives"),
-        .package(path: "../swift-test-primitives"),
+        .package(path: "../swift-numeric-primitives")
     ],
     targets: [
         .target(
@@ -31,17 +30,9 @@ let package = Package(
                 .product(name: "Algebra Linear Primitives", package: "swift-algebra-linear-primitives"),
                 .product(name: "Affine Primitives", package: "swift-affine-primitives"),
                 .product(name: "Dimension Primitives", package: "swift-dimension-primitives"),
-                .product(name: "Real Primitives", package: "swift-numeric-primitives"),
+                .product(name: "Real Primitives", package: "swift-numeric-primitives")
             ]
-        ),
-        .testTarget(
-            name: "Symmetry Primitives Tests",
-            dependencies: [
-                "Symmetry Primitives",
-                .product(name: "Real Primitives", package: "swift-numeric-primitives"),
-                .product(name: "Test Primitives", package: "swift-test-primitives"),
-            ]
-        ),
+        )
     ],
     swiftLanguageModes: [.v6]
 )
@@ -52,7 +43,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
         .enableExperimentalFeature("Lifetimes"),
-        .strictMemorySafety(),
+        .strictMemorySafety()
     ]
     target.swiftSettings = (target.swiftSettings ?? []) + settings
 }
