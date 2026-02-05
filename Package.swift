6aeb4ac4be3e37a9f410fe37d7f60646d2652d45
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-05 08:52:46 +0100

    Add algebra-group-primitives for Phase group operations

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../swift-algebra-linear-primitives"),
+        .package(path: "../swift-algebra-group-primitives"),
         .package(path: "../swift-affine-primitives"),
         .package(path: "../swift-dimension-primitives"),
         .package(path: "../swift-numeric-primitives")
@@ -28,6 +29,7 @@ let package = Package(
             name: "Symmetry Primitives",
             dependencies: [
                 .product(name: "Algebra Linear Primitives", package: "swift-algebra-linear-primitives"),
+                .product(name: "Algebra Group Primitives", package: "swift-algebra-group-primitives"),
                 .product(name: "Affine Primitives", package: "swift-affine-primitives"),
                 .product(name: "Dimension Primitives", package: "swift-dimension-primitives"),
                 .product(name: "Real Primitives", package: "swift-numeric-primitives")
